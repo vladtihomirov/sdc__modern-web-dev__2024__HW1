587f3ca4b1f28e0aa697e89540c45d4d490db797
vlad.tihomirov <tsikhamirau.uladzislau@student.ehu.lt>
2025-01-14 16:19:45 +0000

    fix: 🐛 HW-4 - Fix getCategories

--- src/services/MenuService.ts
+++ src/services/MenuService.ts
@@ -3,11 +3,9 @@ import {IMenuItem} from "../@types/IMenuItem.ts";
 
 export class MenuService extends BaseService {
   public static getCategories(): Promise<string[]> {
-    return Promise.resolve([
-      "Dessert",
-      "Dinner",
-      "Breakfast"
-    ]);
+    return this.get<IMenuItem[]>('meals').then((data => {
+      return Array.from(new Set(data.map(item => item.category)));
+    }));
   }
 
   public static getMenuItems(): Promise<IMenuItem[]> {
